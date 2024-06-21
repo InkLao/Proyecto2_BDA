51dceb0ce56393530970d3f7c20b4f3ed8d4ead5
ArturoITSON <145297339+ArturoITSON@users.noreply.github.com>
2024-06-20 19:15:53 -0700

    Creación de algunas frames de presentación
    - Administrar pagos
    - Cambiar estatus
    - Iniciar sesion
    - Inicio Admin
    - Inicio Usuario
    - Nuevo Pago

---
+++ PagoBeneficiarioPresentacion/src/main/java/com/mycompany/pagobeneficiariopresentacion/PagoBeneficiarioPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pagobeneficiariopresentacion;
+
+/**
+ *
+ * @author Arturo ITSON
+ */
+public class PagoBeneficiarioPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
