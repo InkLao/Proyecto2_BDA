ffbf0f041c500d179c08ec76e9db143061102969
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-06-21 00:03:13 -0700

    Clases DTO
    - Se definieron las siguientes clases DTO
    - Abono
    - Beneficiario
    - CuentaBancaria
    - Estatus
    - Pago
    - PagoEstatus
    - Tipo

---
+++ pagoBeneficiariosPersistencia/src/main/java/DTO/PagoDTO.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+import java.util.Date;
+
+/**
+ *
+ * @author eduar
+ */
+public class PagoDTO {
+    private Long id;
+    private Date fechaHora;
+    private Double monto;
+    private String comprobante;
+    private Long beneficiarioId;
+    private Long tipoId;
+
+    public PagoDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public String getComprobante() {
+        return comprobante;
+    }
+
+    public void setComprobante(String comprobante) {
+        this.comprobante = comprobante;
+    }
+
+    public Long getBeneficiarioId() {
+        return beneficiarioId;
+    }
+
+    public void setBeneficiarioId(Long beneficiarioId) {
+        this.beneficiarioId = beneficiarioId;
+    }
+
+    public Long getTipoId() {
+        return tipoId;
+    }
+
+    public void setTipoId(Long tipoId) {
+        this.tipoId = tipoId;
+    }
+}
---
+++ pagoBeneficiariosPersistencia/src/main/java/DTO/PagoEstatusDTO.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+import java.util.Date;
+
+/**
+ *
+ * @author eduar
+ */
+public class PagoEstatusDTO {
+    private Long id;
+    private Date fechaHora;
+    private String mensaje;
+    private Long pagoId;
+    private Long estatusId;
+
+    public PagoEstatusDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public Long getPagoId() {
+        return pagoId;
+    }
+
+    public void setPagoId(Long pagoId) {
+        this.pagoId = pagoId;
+    }
+
+    public Long getEstatusId() {
+        return estatusId;
+    }
+
+    public void setEstatusId(Long estatusId) {
+        this.estatusId = estatusId;
+    }
+}
