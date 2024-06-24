994b10759935f6337b76fb9bfd901e061715402d
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-06-23 18:59:37 -0700

    Eliminación de las clases Main
    - Los Frm ya no tienen clase Main, ahora se visualizaran a través de la clase main del proyecto

--- PagoBeneficiarioPresentacion/src/main/java/GUI/frmIniciarSesion.java
+++ PagoBeneficiarioPresentacion/src/main/java/GUI/frmIniciarSesion.java
@@ -219,26 +219,6 @@ private void btnAdminActionPerformed(java.awt.event.ActionEvent evt) {
         }
     }
 
-  public static void main(String args[]) {
-      
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmIniciarSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new frmIniciarSesion().setVisible(true);
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdmin;
     private javax.swing.JButton btnIngresar;
--- PagoBeneficiarioPresentacion/src/main/java/com/mycompany/pagobeneficiariopresentacion/PagoBeneficiarioPresentacion.java
+++ PagoBeneficiarioPresentacion/src/main/java/com/mycompany/pagobeneficiariopresentacion/PagoBeneficiarioPresentacion.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.pagobeneficiariopresentacion;
 
+import GUI.frmInicioUsuario;
+
 /**
  *
  * @author Arturo ITSON
@@ -11,6 +13,8 @@ package com.mycompany.pagobeneficiariopresentacion;
 public class PagoBeneficiarioPresentacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        
+        frmInicioUsuario is = new frmInicioUsuario();
+        is.setVisible(true);
+}
 }
